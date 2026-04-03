ee77c6f7c5a44f8b91c2bbf7032eb2055fc88823
MoWe97 <73847887+MoWe97@users.noreply.github.com>
2026-04-03 16:36:01 +0200

    feat: setup testing on the phone

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
